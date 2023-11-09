2811d0e4a8e2fdb91be6a051fc5bd68d3ead8d50
Kyle Horton <kyle@cplventures.com>
2023-11-09 11:48:31 -0500

    styles on main page

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Home Page</h1>
-<p>Visit <a href="/negroni">Drink 1</a>to see the recipe</p>
+<body>
+  <main>
+    <input type="text" placeholder="Search for drink" />
+    <br />
+    <ul>
+      <li>Drink 1: Lorem ipsum dolor sit amet...</li>
+      <li>Drink 2: Lorem ipsum dolor sit amet...</li>
+      <li>Drink 3: Lorem ipsum dolor sit amet...</li>
+    </ul>
+  </main>
+
+  <footer>
+    <a href="/">Add a new drink</a>
+  </footer>
+</body>
+
+<style>
+  body {
+    display: flex;
+    height: 100vh;
+    flex-direction: column;
+    margin: auto;
+  }
+  main {
+    padding: 1em;
+    flex-grow: 1;
+    text-align: center;
+  }
+  input {
+    width: 100%;
+  }
+  ul {
+    padding: 3em 1em;
+  }
+  li {
+    padding: 0.5em 0;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  footer {
+    text-align: center;
+
+    padding: 2em;
+  }
+</style>
